e0193776abe3ae515f4dcb4f1bc9e0fed39f7b54
Eric Huang <erichuang@meta.com>
2025-06-02 10:52:16 -0700

    impl
    # What does this PR do?
    
    
    ## Test Plan

--- llama_stack/providers/registry/files.py
+++ llama_stack/providers/registry/files.py
@@ -4,8 +4,21 @@
 # This source code is licensed under the terms described in the LICENSE file in
 # the root directory of this source tree.
 
-from llama_stack.providers.datatypes import ProviderSpec
+from llama_stack.providers.datatypes import (
+    Api,
+    InlineProviderSpec,
+    ProviderSpec,
+)
+from llama_stack.providers.utils.kvstore import kvstore_dependencies
 
 
 def available_providers() -> list[ProviderSpec]:
-    return []
+    return [
+        InlineProviderSpec(
+            api=Api.files,
+            provider_type="inline::meta-reference",
+            pip_packages=[] + kvstore_dependencies(),
+            module="llama_stack.providers.inline.files.meta_reference",
+            config_class="llama_stack.providers.inline.files.meta_reference.config.MetaReferenceFilesImplConfig",
+        ),
+    ]

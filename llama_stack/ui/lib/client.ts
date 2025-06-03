72de9b8619f54db20d483674dcef6a6a979289ac
Eric Huang <erichuang@meta.com>
2025-06-03 11:22:58 -0700

    k8s
    # What does this PR do?
    
    
    ## Test Plan

--- llama_stack/ui/lib/client.ts
+++ llama_stack/ui/lib/client.ts
@@ -1,5 +1,5 @@
 import LlamaStackClient from "llama-stack-client";
 
 export const client = new LlamaStackClient({
-  baseURL: process.env.NEXT_PUBLIC_LLAMA_STACK_BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_LLAMA_STACK_BASE_URL + "/api",
 });
